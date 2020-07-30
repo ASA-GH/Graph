5223aa1f71405e260695edb8d41aa1678a3d9dd1
ASA-GH <samorodov21@gmail.com>
2020-07-30 23:46:43 +0700

    d3

--- src/components/GraphApp/Graph/Graphs.jsx
+++ src/components/GraphApp/Graph/Graphs.jsx
@@ -1,10 +1,182 @@
-import React from "react";
-import { Line } from 'react-chartjs-2';
+import React, { useRef, useEffect, useState } from "react";
 import Card from '../Card/Card'
 import { useSelector } from 'react-redux'
+import {
+  select,
+  scaleLinear,
+  line,
+  max,
+  curveCardinal,
+  axisBottom,
+  axisLeft,
+  zoom,
+  zoomTransform
+} from "d3";
+import useResizeObserver from "./useResizeObserver";
+
+
+
+
+
 
 const Graphs = (props) => {
+
   const chips = useSelector(state => state)
+  const createData = (chips) => {
+    let data = {
+      dataset: []
+    };
+    chips.map((chip) => {
+      if (!chip.error) {
+        let item = {
+          label: chip.title,
+          data: chip.data[0].data,
+        };
+
+        data.dataset.push(item);
+      }
+    })
+    return data;
+  };
+
+   // const data = createData(chips);
+const GetMax = (dataset) => {
+  let result = 0;
+  dataset.map((item) => {
+    const m = max(item.data);
+    if (result < m)
+      result = m;
+  });
+  return result;
+}
+
+const GetLenght = (dataset) => {
+  if (!dataset.length)
+    return 0;
+  return dataset[0].data.length - 1;
+}
+
+const data = {
+  dataset: [
+    {
+    label: 'test1',
+    data: [40, 50, 30, 40, 70]
+    },
+    {
+      label: 'test2',
+      data: [45, 55, 35, 45, 60]
+      }
+  ]
+}
+
+
+
+    const svgRef = useRef();
+    const wrapperRef = useRef();
+    const dimensions = useResizeObserver(wrapperRef);
+    const [currentZoomState, setCurrentZoomState] = useState();
+  
+    // will be called initially and on every data change
+    useEffect(() => {
+
+      data.dataset.map((item) => {
+      const svg = select(svgRef.current);
+      const svgContent = svg.select(".content");
+      const { width, height } =
+        dimensions || wrapperRef.current.getBoundingClientRect();
+  
+      // scales + line generator
+      const xScale = scaleLinear()
+        .domain([0, GetLenght(data.dataset)])
+        .range([10, width - 10]);
+  
+      if (currentZoomState) {
+        const newXScale = currentZoomState.rescaleX(xScale);
+        xScale.domain(newXScale.domain());
+      }
+  
+      const yScale = scaleLinear()
+        .domain([0, GetMax(data.dataset)])
+        .range([height - 20, 20]);
+  
+      const lineGenerator = line()
+        .x((d, index) => xScale(index))
+        .y(d => yScale(d))
+        .curve(curveCardinal);
+  
+      // render the line
+
+   
+        svgContent
+        .selectAll(".myLine")
+        .data([item.data])
+        .join("path")
+        .attr("class", "myLine")
+        .attr("stroke", "black")
+        .attr("fill", "none")
+        .attr("d", lineGenerator);
+  
+      svgContent
+        .selectAll(".myDot")
+        .data(item.data)
+        .join("circle")
+        .attr("class", "myDot")
+        .attr("stroke", "black")
+        .attr("r", 4)
+        .attr("fill", "orange")
+        .attr("cx", (value, index) => xScale(index))
+        .attr("cy", yScale);
+   
+     /* svgContent
+        .selectAll(".myLine")
+        .data([data.data])
+        .join("path")
+        .attr("class", "myLine")
+        .attr("stroke", "black")
+        .attr("fill", "none")
+        .attr("d", lineGenerator);
+  
+      svgContent
+        .selectAll(".myDot")
+        .data(data.data)
+        .join("circle")
+        .attr("class", "myDot")
+        .attr("stroke", "black")
+        .attr("r", 4)
+        .attr("fill", "orange")
+        .attr("cx", (value, index) => xScale(index))
+        .attr("cy", yScale);
+  */
+      // axes
+      const xAxis = axisBottom(xScale);
+      svg
+        .select(".x-axis")
+        .attr("transform", `translate(0, ${height})`)
+        .call(xAxis);
+  
+      const yAxis = axisLeft(yScale);
+      svg.select(".y-axis").call(yAxis);
+  
+
+      // zoom
+      const zoomBehavior = zoom()
+        .scaleExtent([0.5, 5])
+        .translateExtent([
+          [0, 0],
+          [width, height]
+        ])
+        .on("zoom", () => {
+          const zoomState = zoomTransform(svg.node());
+          setCurrentZoomState(zoomState);
+        });
+
+  
+      svg.call(zoomBehavior);
+    });
+    }, [currentZoomState, data.data, dimensions]);
+  
+   
+ /* const chips = useSelector(state => state)
   const createData = (chips) => {
     let data = {
       labels: [100, 200, 300, 400, 600, 700, 800, 900, 1000],
@@ -38,11 +210,22 @@ const Graphs = (props) => {
       }
     })
     return data;
-  }
+  }*/
 
   return (
     <Card wrapperGraphs>
-      <Line data={createData(chips)} options={{ maintainAspectRatio: false }} />
+<div ref={wrapperRef} style={{ marginBottom: "2rem" }}>
+        <svg ref={svgRef}>
+          <defs>
+            <clipPath id={data.label}>
+              <rect x="0" y="0" width="100%" height="100%" />
+            </clipPath>
+          </defs>
+          <g className="content" clipPath={`url(#${data.label})`}></g>
+          <g className="x-axis" />
+          <g className="y-axis" />
+        </svg>
+      </div>
     </Card>
   );
 
