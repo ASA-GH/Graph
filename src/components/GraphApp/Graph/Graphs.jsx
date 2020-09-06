1dab16669f3fe5d019b0b8ec454e07bda91b9e4b
ASA-GH <samorodov21@gmail.com>
2020-09-06 23:17:46 +0700

    This is victory

--- src/components/GraphApp/Graph/Graphs.jsx
+++ src/components/GraphApp/Graph/Graphs.jsx
@@ -1,7 +1,13 @@
 import React, { useRef, useEffect, useState } from "react";
 import Card from '../Card/Card'
 import { useSelector } from 'react-redux'
-import { VictoryChart, VictoryArea, VictoryTheme, VictoryAxis, VictoryLine } from "victory";
+import { VictoryChart, 
+         VictoryArea, 
+         VictoryTheme,
+         VictoryVoronoiContainer,
+         VictoryTooltip, 
+         VictoryAxis, 
+         VictoryLine } from "victory";
 import useResizeObserver from "./useResizeObserver";
 import Timer from '../Store/Timer'
 
@@ -20,48 +26,46 @@ let dataState = State[0].data;
 console.log(State)
 
 
-let f = (data) => {
+let normalization = (data) => {
   let result = [];
   for (const [index, value] of data.entries()) {
 
     let obj = JSON.parse(value);
-    let obj1 = {x: new Date(obj.x.parseInt()), y:(obj.y)};
-    console.log(obj1);
+    let obj1 = {x: new Date (Number(obj.x)), y:Number(obj.y)};
                 
     result.push(obj1);
   }
-   
-  console.log(result);
+   console.log(result);
   return result;
-  
-}
+ } 
+// console.log(a);
+// const a =f(sampleData.threeData)
 
 //console.log(f(arrDate, arrValue));
-console.log("---------------------------------");
-console.log('');
-console.log('color');
+// console.log("---------------------------------");
+// console.log('');
+// console.log('color');
 
-console.log(dataState.dataset.length ? dataState.dataset[0].color : '!C!');
+// console.log(dataState.dataset.length ? dataState.dataset[0].color : '!C!');
 
-console.log('');
-console.log('data');
+// console.log('');
+// console.log('data');
 
-console.log(dataState.dataset.length ? dataState.dataset[0].data: '!D!');
-console.log('');
+// console.log(dataState.dataset.length ? dataState.dataset[0].data: '!D!');
+// console.log('');
 
-console.log('label');
-console.log(dataState.dataset.length ? dataState.dataset[0].label : '!L!');
-console.log('');
+// console.log('label');
+// console.log(dataState.dataset.length ? dataState.dataset[0].label : '!L!');
+// console.log('');
 
-console.log('scale');
-console.log(dataState.scale.length ? dataState.scale : 0 +', ' + 1);
-console.log('');
+// console.log('scale');
+// console.log(dataState.scale.length ? dataState.scale : 0 +', ' + 1);
+// console.log('');
 
-console.log('dataState');
-console.log(dataState);
-console.log('');
-scale:
-console.log("---------------------------------");
+// console.log('dataState');
+// console.log(dataState);
+// console.log('');
+// console.log("---------------------------------");
 
 const stopTimer = (e) => {setStop(true)}; 
 const startTimer = (e) => {setStop(false)};       
@@ -70,8 +74,6 @@ const startTimer = (e) => {setStop(false)};
     const wrapperRef = useRef();
     // const dimensions = useResizeObserver(wrapperRef);
     const [stop, setStop] = useState(true);
-
-
 
      let sampleData = {
        oneData:[
@@ -86,31 +88,23 @@ const startTimer = (e) => {setStop(false)};
       { x: 9, y: 3 }
     ],
     twoleData:[
-      { x: 0, y: 10 },
-      { x: 1, y: 10 },
-      { x: 2, y: 5 },
-      { x: 3, y: 8 },
-      { x: 4, y: 4 },
-      { x: 5, y: 0 },
-      { x: 6, y: 1 },
-      { x: 7, y: 6 },
-      { x: 8, y: 2 },
-      { x: 9, y: 6 },
-      { x: 10, y: 9 }
+      { x: new Date(1598803819000), y: 6 },
+      { x: new Date(1598803820000), y: 1 },
+      { x: new Date(1598803821000), y: 0 },
+      { x: new Date(1598803822000), y: 1 },
+      { x: new Date(1598803823000), y: 10 },
+      { x: new Date(1598803824000), y: 18 },
+      { x: new Date(1598803825000), y: 10 },
+      { x: new Date(1598803826000), y: 5 }
+
     ],
     threeData:[
-      { x: 0, y: 2 },
-      { x: 1, y: 1 },
-      { x: 2, y: 2 },
-      { x: 3, y: 3 },
-      { x: 4, y: 0 },
-      { x: 5, y: 4 },
-      { x: 6, y: 9 },
-      { x: 7, y: 6 },
-      { x: 8, y: 9 }
+      { x: 1598803819466, y: 4 },
+      { x: 1598803849566, y: 14 },
+      { x: 1598803869666, y: 1 },
+      { x: 1598803889766, y: 15 }
     ]
   }
-
        
   return (
 <Card wrapperGraphs>
@@ -121,79 +115,72 @@ const startTimer = (e) => {setStop(false)};
 </div>
   <div>
     <svg width={2000} height={500}>
-   
-  {/* <VictoryLine
-  interpolation="natural"
-  // 
-  data={sampleData}
-/> */}
-{/* <circle cx={150} cy={150} r={150} fill="#c43a31"/> */}
   <VictoryChart
+  scale={{ x: "time" , y:"linear"}}
+  // domain={{ y: [0, 20] }}
+  // y0={0}
     standalone={false}
     width={1500} height={500}
     theme={VictoryTheme.material}
     minDomain={{ y: 0 }}
-    
-  >
-   {/* <VictoryAxis 
+    // containerComponent={
+    //   <VictoryVoronoiContainer voronoiDimension="x"
+    //     labels={({ datum }) => `y: ${datum.y}`}
+    //     labelComponent={<VictoryTooltip cornerRadius={0} flyoutStyle={{fill: "white"}}/>}/>}
+      >
+  {/* <VictoryAxis dependentAxis
+  orientation="left"
+/> */}
+
+    <VictoryAxis 
    
-     orientation="bottom"
-    // tickValues={dataState.scale.length ? dataState.scale : [0 , 1]}
-    tickValues={arrDate}
+     orientation="bottom"/>
+   {/*   // tickValues={dataState.scale.length ? dataState.scale : [0 , 1]}
+   tickValues={arrDate}
 
     // domain={[0, 10]}
     theme={VictoryTheme.material}
     offsetY={40}
     offsetX={0}
     standalone={false}
-  />
-  <VictoryAxis 
+  /> */}
+  <VictoryAxis dependentAxis
      orientation="left"
-    domain={[0, 20]}
-    tickValues={arrValue}
+    // tickValues={[0, 10,20]}
 
-    theme={VictoryTheme.material}
-    offsetX={40}
-    offsetY={0}
-    standalone={false}
-  /> */}
+    // standalone={false}
+  /> 
+{
+dataState.dataset.map(obj => (
 
- {/* <VictoryLine
-  interpolation="natural"
-  style={{
-    data: {
-       stroke: dataState.dataset.length ? dataState.dataset[0].color : '!1!!', strokeWidth: 3
-    }}}
-  standalone={false}
-  // x={null} y={null}
-  data={sampleData.oneData}/> */}
 <VictoryLine
-  interpolation="natural"
-  standalone={false}
+  // labelComponent = {<VictoryTooltip />}  
+  // interpolation="monotoneX"
+  interpolation="linear"
+  // y0={0}
+  scale={{ x: "time" , y:"linear"}}
+
+  // standalone={false}
   style={{
+    // data: {
+    //    stroke: "#77DDE7", strokeWidth: ({ active }) => active ? 4 : 2},
+    //    labels: {fill: "tomato"}
+    // 
     data: {
-       stroke: "#77DDE7", strokeWidth: 3
-    }}}
+      stroke: obj.color, strokeWidth: 3}}}
   // x={null} y={null}
-  data={dataState.dataset.length ? f(dataState.dataset[0].data): []}/>
-  {/* <VictoryLine
-  // interpolation="natural"
-  standalone={false}
-  // x={null} y={null}
-  data={sampleData.threeData}
-  style={{
-    data: {
-       stroke: "#E32636", strokeWidth: 6
-    }
-    // labels: {
-    //   fontSize: 15, fill: "#c43a31", padding: 15
-    // }
-  }}
-  // labels={({ datum }) => datum.x}
   
-  
-  /> */}
-  </VictoryChart>
+    // labels={({ datum }) => datum.y}
+    data = {normalization(obj.data)}
+    // data = {sampleData.twoleData}
+
+    
+    />
+
+  ))
+
+}
+ </VictoryChart>
 </svg>
 
   </div>
