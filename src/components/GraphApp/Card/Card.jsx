71eaa5fe148b57d6f16210ef42139d6df28a4991
ASA-GH <samorodov21@gmail.com>
2020-09-23 22:47:46 +0700

    Refactoring Card.css, Card.jsx

--- src/components/GraphApp/Card/Card.jsx
+++ src/components/GraphApp/Card/Card.jsx
@@ -3,37 +3,27 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames'
 import './Card.css';
 
-const Card = ({
-  className,
-  tag: Tag,
-  ...attrs
-}) => {
+const Card = ({className, tag: Tag, ...attrs}) => {
   const classes = classNames(
     className,
     { wrapperGraphApp: attrs.wrapperGraphApp },
     { wrapperGraphs: attrs.wrapperGraphs },
     { wrapperGraphData: attrs.wrapperGraphData },
     { wrapperChips: attrs.wrapperChips },
-    // { innerChips: attrs.innerChips },
+    { innerChips: attrs.innerChips },
     { wrapperControl: attrs.wrapperControl },
-    { wrapperTimer: attrs.wrapperTimer },
-    { victoryChart: attrs.victoryChart }
-
+    { wrapperTimer: attrs.wrapperTimer }
   );
-
   return (
     <Tag className={classes} {...attrs} />
   );
 };
-
 Card.propTypes = {
   className: PropTypes.string,
   tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
 };
-
 Card.defaultProps = {
   className: '',
   tag: 'div',
 };
-
 export default Card;
